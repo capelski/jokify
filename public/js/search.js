26139bb4d5d18a7eecd31f728bb7461b5a465796
Carles Capellas <capellas.carles@gmail.com>
2019-12-19 12:02:36 +0100

    feat: add search page deep link functionality

--- public/js/search.js
+++ public/js/search.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var filter = document.getElementById('filter');
 
     function getFilteredJokes() {
+        const updatedUrl = window.location.href.split('?')[0] + `?text=${filter.value}`;
+        window.history.replaceState(null, null, updatedUrl);
         animateJoke(jokes);
         randomColorize();
         animateSymbols();
@@ -22,6 +24,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
             jokes.scrollTo({ top: 0 });
         });
     }
+
+    // Check for the first url parameter and perform a search with it's value
+    if (window.location.search) {
+        const textValue = window.location.search.substr(1).split('=')[1];
+        filter.value = textValue;
+        getFilteredJokes();
+    }
+
+    function keyDownTextField(e) {
+        if(e.target === filter && e.keyCode === 13) {
+            getFilteredJokes();
+        }
+    }
+    document.addEventListener('keydown', keyDownTextField, false);
 
     searcher.addEventListener('click', getFilteredJokes);
     randomColorize();
