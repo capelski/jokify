ad897ff01881e723c815d91b53a6f746c5e15614
Carles Capellas <capellas.carles@gmail.com>
2020-02-19 09:23:55 +0100

    feat: add pseudo server side rendering

---
+++ client/components/app.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, useHistory } from 'react-router-dom';
+import { useSwipeable } from 'react-swipeable';
+import { Joke } from '../types';
+import { fetchServerJoke, getRandomTheme } from '../utils';
+import { Buttons } from './buttons';
+import { Emojis } from './emojis';
+import { Jokes } from './jokes';
+
+// If calling the function directly in the useState, it will be called in every render cycle
+const initialTheme = getRandomTheme();
+
+// tslint:disable-next-line:variable-name
+const AppWithHistory = () => {
+    const history = useHistory();
+
+    // For the application to work on both http://localhost/ and http://domain/jokify/
+    const [baseUrl] = useState(window.location.pathname.indexOf('/jokify') > -1 ? '/jokify/' : '/');
+
+    const [jokes, setJokes] = useState<Joke[]>([]);
+    const [jokeIndex, setJokeIndex] = useState(-1);
+
+    const [animationDirection, setAnimationDirection] = useState('slide-left');
+    const [filter, setFilter] = useState('');
+    const [isFilterVisible, setIsFilterVisible] = useState(false);
+    const [theme, setTheme] = useState(initialTheme);
+
+    const fetchJoke = (id?: Joke['id']) => {
+        fetchServerJoke(id, isFilterVisible ? filter : undefined)
+            .then(response => {
+                setJokes([...jokes, response.data]);
+                updateCurrentJoke(jokes.length, response.data.id);
+            })
+            .catch(() => {});
+    };
+
+    const nextJoke = () => {
+        setAnimationDirection('slide-left');
+
+        if (jokeIndex < jokes.length - 1) {
+            const nextIndex = jokeIndex + 1;
+            updateCurrentJoke(nextIndex, jokes[nextIndex].id);
+        } else {
+            fetchJoke();
+        }
+    };
+
+    const previousJoke = () => {
+        setAnimationDirection('slide-right');
+
+        const nextIndex = Math.max(jokeIndex - 1, 0);
+        updateCurrentJoke(nextIndex, jokes[nextIndex].id);
+    };
+
+    const updateCurrentJoke = (nextIndex: number, jokeId: number) => {
+        setJokeIndex(nextIndex);
+        setTheme(getRandomTheme());
+        history.push(`${baseUrl}${jokeId}`);
+    };
+
+    const onKeyDown = (event: React.KeyboardEvent) => {
+        if (event.keyCode === 37) {
+            previousJoke();
+        } else if (event.keyCode === 39) {
+            nextJoke();
+        }
+    };
+
+    useEffect(() => {
+        // Set the focus to the viewport to enable the key events
+        document.querySelector<HTMLDivElement>('.viewport')!.focus();
+    }, []);
+
+    const swipeHandlers = useSwipeable({ onSwipedLeft: nextJoke, onSwipedRight: previousJoke });
+
+    return (
+        <div {...swipeHandlers} className={`viewport ${theme}`} tabIndex={0} onKeyDown={onKeyDown}>
+            <Jokes
+                animationDirection={animationDirection}
+                currentIndex={jokeIndex}
+                fetchJoke={fetchJoke}
+                isFilterVisible={isFilterVisible}
+                jokes={jokes}
+            />
+            <Buttons
+                isFilterVisible={isFilterVisible}
+                joke={jokes[jokeIndex]}
+                nextJoke={nextJoke}
+                onFilterChange={setFilter}
+                previousJoke={previousJoke}
+                setIsFilterVisible={setIsFilterVisible}
+            />
+            <Emojis />
+
+            {/* Preload the background images so there is no flickering on the first theme load */}
+            <div style={{ backgroundImage: 'url("/images/alpha-bg.png?$modena=jokify")' }} />
+            <div style={{ backgroundImage: 'url("/images/beta-bg.png?$modena=jokify")' }} />
+            <div style={{ backgroundImage: 'url("/images/gamma-bg.png?$modena=jokify")' }} />
+            <div style={{ backgroundImage: 'url("/images/delta-bg.png?$modena=jokify")' }} />
+        </div>
+    );
+};
+
+// tslint:disable-next-line:variable-name
+export const App = () => (
+    <BrowserRouter>
+        <AppWithHistory />
+    </BrowserRouter>
+);
--- server/get-express-app.ts
+++ server/get-express-app.ts
@@ -1,9 +1,50 @@
+import axios from 'axios';
 import express from 'express';
+import { readFile } from 'fs';
 import { join } from 'path';
 
 export default () => {
     const app = express();
-    app.use('/', express.static(__dirname));
-    app.use('/:id', (req, res) => res.sendFile(join(__dirname, 'index.html')));
+
+    app.use(express.static(__dirname));
+    app.use('/:id', (req, res) => {
+        const { id } = req.params;
+
+        return axios
+            .get(`/joke/${id}?$modena=jokify-api`)
+            .then(response => {
+                readFile(join(__dirname, 'index.html'), 'utf8', (error, fileContents) => {
+                    if (error) {
+                        console.error(error);
+                        return res.status(500).send('An error occurred');
+                    }
+
+                    // Ideally, we would replace the following div with the server rendered version of the app.
+                    // Given we are using BrowserRouter however, we will settle with sending part of the html
+                    const replacedHtml = fileContents
+                        .replace(
+                            '<div id="app-placeholder"></div>',
+                            `<div id="app-placeholder">
+<div class="joke">${response.data.text
+                                .map(
+                                    (paragraph: string) =>
+                                        `<p class="joke-paragraph">${paragraph}</p>`
+                                )
+                                .join('')}</div>
+</div>`
+                        )
+                        .replace(
+                            /<meta name="description" [^>]*\/>/,
+                            `<meta name="description" content="${response.data.text.join(
+                                ' / '
+                            )}" />`
+                        );
+
+                    return res.send(replacedHtml);
+                });
+            })
+            .catch(() => res.status(500).send('An error occurred'));
+    });
+
     return app;
 };
