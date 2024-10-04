7f0c1a5d28c5548e2b706bccc451dddc299c344a
Carles Capellas <capellas.carles@gmail.com>
2024-10-04 22:15:41 +0200

    Configure capacitor

---
+++ webpack/www.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const baseConfig = require('./base.config');
+
+module.exports = merge(baseConfig, {
+    mode: 'production',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '..', 'www'),
+        publicPath: '/'
+    },
+});
