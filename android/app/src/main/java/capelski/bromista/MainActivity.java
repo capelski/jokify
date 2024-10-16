86da2a85156d4c79455f6623687c5e83499e7e16
Carles Capellas <capellas.carles@gmail.com>
2024-10-16 23:13:04 +0200

    Rename the app to Bromista

--- android/app/src/main/java/capelski/bromista/MainActivity.java
+++ android/app/src/main/java/capelski/bromista/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jokify.app;
+package capelski.bromista;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jokify.app',
-  appName: 'jokify',
+  appId: 'capelski.bromista',
+  appName: 'Bromista',
   webDir: 'www'
 };
 
